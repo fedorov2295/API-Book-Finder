5bb13b8da3ba8282d777c2f1fe7594496dc88e60
SERGEI FEDOROV <72577233+fedorov2295@users.noreply.github.com>
2021-05-17 22:01:45 +0300

    Addition of Extended Snippet

--- src/App.js
+++ src/App.js
@@ -2,7 +2,9 @@ import React, { useState } from 'react'
 import classes from './App.module.css';
 import Search from './components/Search/Search';
 import Book from './components/Books/Book/Book';
-import Spinner from './UI/Spinner/Spinner'
+import Spinner from './UI/Spinner/Spinner';
+import Modal from './UI/Modal/Modal';
+import SnippetExtended from './components/SnippetExtended/SnippetExtended';
 
 
 const App = props => {
@@ -10,39 +12,55 @@ const App = props => {
   const [loading, setLoading] = useState(false);
   const [books, setBooks] = useState([]);
   const [errorMessage, setErrorMessage] = useState(null);
+  const [show, showModal] = useState(false);
+  const [snippetBook, setsnippetBook] = useState({})
+
+  const clickSnippetHandler = (book) => {
+    showModal(true);
+    const target = book.target.localName === "p" || book.target.localName === "img" ? book.target.parentElement : book.target
+    setsnippetBook(books[target.id]);
+    console.log(book)
+  }
+
+  const exitExtendedSnippet = () => {
+    showModal(false)
+  }
 
   const search = searchValue => {
     setLoading(true);
     setErrorMessage(null);
-  
 
-  fetch(`http://openlibrary.org/search.json?title=${searchValue}`)
+
+    fetch(`http://openlibrary.org/search.json?title=${searchValue}`)
       .then(response => response.json())
       .then(jsonResponse => {
-        
         if (jsonResponse.numFound > 0) {
           setBooks(jsonResponse.docs);
           setLoading(false);
-          console.log(jsonResponse.docs[1].title)
-        console.log(jsonResponse.docs[1].author_name.join(','))
+          console.log(jsonResponse.docs)
         } else {
           setErrorMessage(jsonResponse.Error);
           setLoading(false);
         }
       });
-  	};
+  };
 
   return (
     <div className={classes.App}>
-      <Search search={search}/>
+      {books ? <Modal show={show} modalClosed={exitExtendedSnippet}>
+                <SnippetExtended book={snippetBook} />
+      </Modal>: null}
+      <Search search={search} />
       <div className={classes.Main}>
-      {loading && !errorMessage ? (
-         <Spinner />
-         ) : errorMessage ? (
+        {loading && !errorMessage ? (
+          <Spinner />
+        ) : errorMessage ? (
           <div className="errorMessage">{errorMessage}</div>
         ) : (
           books.map((book, index) => (
-            <Book key={`${index}-${book.title}`} title={book.title} author={book.author_name} />
+            <div>
+              <Book snippetClicked={clickSnippetHandler} key={index} id={index} book={book} />
+            </div>
           ))
         )}
       </div>
--- src/components/Books/Book/Book.js
+++ src/components/Books/Book/Book.js
@@ -2,14 +2,19 @@ import React from 'react';
 import classes from './Book.module.css';
 
 const Book = (props) => {
-    
 
-    
-    return(
-        <div className={classes.Book}>
-            <p><strong>Book Title:</strong> {props.title}</p>
-            <p><strong>Author:</strong> {props.author}</p>
-        </div>
+    const cover = props.book.isbn ? props.book.isbn[1] : null
+    const bookCoverSrc = `http://covers.openlibrary.org/b/isbn/${cover}-S.jpg`
+
+    return (
+        <span 
+        className={classes.Book}
+        onClick={props.snippetClicked}
+        id={props.id}>
+            <img src={bookCoverSrc} />
+            <p>Book Title: {props.book.title}</p>
+            <p>Author: {props.book.author_name ? props.book.author_name.join(',') : null}</p>
+        </span>
     )
 };
 
